4e02245da4fe21ba90985e7535ce0f71261fbba3
Adam Stebbins <acstebbins7@gmail.com>
2021-03-15 20:45:42 -0400

    item16 needs work...

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const par = document.querySelector ('p')
-par.style.color = 'blue'
+const paragraph = document.querySelector ('p')
+paragraph.style.color = 'blue'
 
 
 const heading = document.querySelector('h1')
 heading.style.fontSize = '48px'
-par.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
 
 const list = document.querySelectorAll('li')
@@ -26,5 +26,5 @@ image2.style.height = '300px'
 
 
 const item16 = list[15]
-item16.innerHTML = '<li class="item" id="item-16">Won\'t get fooled again</li>'
+item16.innerHTML = 'li class="item" id="item-16"</li>'
 console.log(item16)
