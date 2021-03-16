ecbc78fea41df89a6ae1f56a60de8b316648863b
Adam Stebbins <acstebbins7@gmail.com>
2021-03-15 22:00:29 -0400

    Constanly Styling.

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 const paragraph = document.querySelector ('p')
+
 paragraph.style.color = 'blue'
 
-
 const heading = document.querySelector('h1')
-heading.style.fontSize = '48px'
-paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
+heading.style.fontSize = '48px'
+
+paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
 const list = document.querySelectorAll('li')
 
-
 const item13 = list[12]
+
 item13.style.opacity = 0.5
+
 document.querySelectorAll('li')[2].innerHTML = 'I say, "Hi!"'
 
-
 const image = document.querySelector ('img');
+
 image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+
 image.style.height = '300px'
 
-
 const image2 = document.querySelectorAll('img')[1]
+
 console.log(image2)
+
 image2.style.height = '300px'
 
-
 const liItem16 = document.querySelector("#item-16")
+
 document.querySelector('li')[16].innerHTML = "Won't get fooled again."
